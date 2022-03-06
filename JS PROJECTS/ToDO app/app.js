cd69ce902959e9478ccd0b5c0e31e6bee10fa3eb
SaroshAhmed22 <SaroshAhmed622@gmail.com>
2022-03-07 01:34:10 +0500

    Todo app complete

---
+++ JS PROJECTS/ToDO app/app.js
@@ -0,0 +1,64 @@
+// console.log('STart')
+
+// ! 👌  function to add Iytem
+function getItem() {
+    let list = document.getElementById('list');
+    let myvalue = document.getElementById('myvalue')
+    let value = myvalue.value;
+    let node = document.createElement("li");
+    // console.log(node)
+    let textnode = document.createTextNode(value);
+    // console.log(textnode);
+    node.appendChild(textnode);
+    // console.log(node);
+    // console.log(value)
+    list.appendChild(node);
+    myvalue.value=""
+    // ! adding add Btn start
+    let editBtn = document.createElement("button");
+    // console.log(editBtn)
+    editBtn.setAttribute('id', 'edit');
+    editBtn.setAttribute('class', 'editBtn');
+    editBtn.setAttribute('onclick', 'editItem(this)');
+    let editTextNode = document.createTextNode('Edit Item');
+    editBtn.appendChild(editTextNode);
+    node.appendChild(editBtn);
+    // ! adding add Btn end
+
+    // ! adding delete Btn start
+    let deleteBtn = document.createElement("button");
+    deleteBtn.setAttribute('id', 'edit');
+    deleteBtn.setAttribute('class', 'deleteBtn');
+    deleteBtn.setAttribute('onclick', 'deleteItem(this)');
+    let deleteTextNode = document.createTextNode('delete Item');
+    deleteBtn.appendChild(deleteTextNode);
+    node.appendChild(deleteBtn);
+
+    // ! adding delete Btn end
+
+
+
+}
+
+// ! edit item start
+function editItem(e) {
+    let newText = prompt("Please Enter New Text");
+    e.parentNode.firstChild.nodeValue = newText;
+    // console.log(e.parentNode.firstChild)
+    // console.log(e.parentNode.firstChild.nodeValue)
+    // e.parentNode.firstChild.nodeValue="Sarosh"
+    // console.log(e.parentNode())
+}
+// ! edit item end
+
+// ! delete item start
+
+function deleteItem(e) {
+    e.parentNode.remove();
+
+}
+
+// ! delete item end
+
+
+
