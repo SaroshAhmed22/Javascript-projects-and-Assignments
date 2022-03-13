be38fc52c6ae006e72102a271e104d6ccc7dc261
SaroshAhmed22 <SaroshAhmed622@gmail.com>
2022-03-13 16:21:12 +0500

    JS class 5

---
+++ Practice/aap2.js
@@ -0,0 +1,31 @@
+// * console.log("first")
+
+const data = [
+    {
+        name: 'Sarosh',
+        age: 20,
+    },
+
+    {
+        name: 'Ali',
+        age: 21,
+    },
+
+    {
+        name: 'Hamza',
+        age: 10,
+    },
+
+    {
+        name: 'kiran',
+        age: 9,
+    },
+]
+
+
+const newData=data.filter((data,index)=>{
+    // return data[5]
+})
+
+
+console.log(newData)
